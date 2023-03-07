02577d34545bd6ed3f1f4fff2f275c37910b04db
Belen-Rodriguez <122569690+Belen-Rodriguez@users.noreply.github.com>
2023-03-07 22:00:41 +0100

    page progets started

--- project/javascript/index.js
+++ project/javascript/index.js
@@ -3,12 +3,10 @@ const nav = document.querySelector("#nav");
 const abrir = document.querySelector("#abrir");
 const cerrar = document.querySelector("#cerrar");
 
-let btnContact = document.querySelector("#contactBt")
-
 abrir.addEventListener("click", () => {
-    nav.innerHTML += btnContact;
     nav.classList.add("visible");
 })
 cerrar.addEventListener("click", () => {
     nav.classList.remove("visible");
+
 })
